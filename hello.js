65041981f1b1536e98041e79e0cbe3db1fd1107a
RuFrontEnd <k718198@gmail.com>
2022-01-14 22:02:09 +0800

    新增 hello.js

---
+++ hello.js
@@ -0,0 +1,13 @@
+// terminal 輸入 node 可以進 REPL(read-eval-print loop) => 類似於瀏覽器開發者工具
+// terminal 輸入 node "file name" => 執行 js 檔
+// global 為 node.js 的頂層物件
+
+const mission = "learn";
+
+if (mission === "learn") {
+  console.log("Time to write some Node code!");
+} else {
+  console.log(`Is" ${mission} "really more fun?`);
+}
+
+// Introduction to Backend VS Frontend
